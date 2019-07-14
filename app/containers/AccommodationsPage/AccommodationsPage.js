840e14afc0041c332e5b5c9a2fece315301cda06
ram757 <rmcdonald144@gmail.com>
2019-07-14 16:33:12 -0400

    moved remainder of gmap content to boguspage

--- app/containers/AccommodationsPage/AccommodationsPage.js
+++ app/containers/AccommodationsPage/AccommodationsPage.js
@@ -7,14 +7,6 @@ import './style.scss';
 export default class AccommodationsPage extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
 
-  static defaultProps = {
-    center: {
-      lng: -73.511592,
-      lat: 41.045260
-    },
-    zoom: 11
-  };
-
   // Since state and props are static,
   // there's no need to re-render this component
   shouldComponentUpdate() {
--- app/containers/BogusPage/BogusPage.js
+++ app/containers/BogusPage/BogusPage.js
@@ -14,6 +14,14 @@ import './style.scss';
 export default class BogusPage extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
 
+  static defaultProps = {
+    center: {
+      lng: -73.511592,
+      lat: 41.045260
+    },
+    zoom: 11
+  };
+
   // Since state and props are static,
   // there's no need to re-render this component
   shouldComponentUpdate() {
