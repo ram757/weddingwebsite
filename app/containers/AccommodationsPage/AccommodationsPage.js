46d5b5ca8964453980319e6094785b018481e79a
ram757 <rmcdonald144@gmail.com>
2019-07-14 16:32:31 -0400

    moved test content to BogusPage

--- app/containers/AccommodationsPage/AccommodationsPage.js
+++ app/containers/AccommodationsPage/AccommodationsPage.js
@@ -1,14 +1,8 @@
 /*
  * AccommodationsPage
- *
- * List of bogus stuff
  */
 import React from 'react';
-// import GoogleMap from 'google-map-react';
-// import { G_MAPS_API_KEY } from '../../hidden';
 import './style.scss';
-
-const TestComponent = ({ text }) => <div>{text}</div>;
 
 export default class AccommodationsPage extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
@@ -30,19 +24,6 @@ export default class AccommodationsPage extends React.Component {
   render() {
     return (
       <div className="accommodations-page">
-        <div style={{ height: '50vh', width: '50%' }}>
-          {/*<GoogleMap*/}
-            {/*bootstrapURLKeys={{ key: G_MAPS_API_KEY }}*/}
-            {/*defaultCenter={AccommodationsPage.defaultProps.center}*/}
-            {/*defaultZoom={AccommodationsPage.defaultProps.zoom}*/}
-          {/*>*/}
-            {/*<TestComponent*/}
-              {/*lat={41.045260}*/}
-              {/*lng={-73.511592}*/}
-              {/*text="BUTTS"*/}
-            {/*/>*/}
-          {/*</GoogleMap>*/}
-        </div>
       </div>
     );
   }
--- app/containers/BogusPage/BogusPage.js
+++ app/containers/BogusPage/BogusPage.js
@@ -5,7 +5,11 @@
  */
 import React from 'react';
 import SomebodySays from '../../components/SomebodySays';
+// import GoogleMap from 'google-map-react';
+// import { G_MAPS_API_KEY } from '../../hidden';
 import './style.scss';
+
+// const TestComponent = ({ text }) => <div>{text}</div>;
 
 export default class BogusPage extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
@@ -62,6 +66,22 @@ export default class BogusPage extends React.Component {
               avatar="Edgar"
               headerText="This one just expands/collapses..."
             />
+          </li>
+          <li>
+            <div style={{ height: '50vh', width: '50%' }}>
+              G-maps dawg
+              {/*<GoogleMap*/}
+                {/*bootstrapURLKeys={{ key: G_MAPS_API_KEY }}*/}
+                {/*defaultCenter={AccommodationsPage.defaultProps.center}*/}
+                {/*defaultZoom={AccommodationsPage.defaultProps.zoom}*/}
+              {/*>*/}
+                {/*<TestComponent*/}
+                  {/*lat={41.045260}*/}
+                  {/*lng={-73.511592}*/}
+                  {/*text="BUTTS"*/}
+                {/*/>*/}
+              {/*</GoogleMap>*/}
+            </div>
           </li>
         </ul>
       </div>
