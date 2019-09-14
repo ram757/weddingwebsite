c3d2a5bdbc5f1b64cf2856cb96d138bff758a382
ram757 <rmcdonald144@gmail.com>
2019-09-14 15:49:59 -0400

    refactored FAQ page and add content

--- app/containers/FAQPage/FAQPage.js
+++ app/containers/FAQPage/FAQPage.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import TitleTextCrunch from '../../components/TitleTextCrunch';
 import EdgarCreation from './images/the_creation_of_edgar.jpg';
+import { faqList } from './faqContent';
 import './style.scss';
 
 export default function FAQPage() {
@@ -11,64 +12,16 @@ export default function FAQPage() {
     <div className="faq-page">
       <TitleTextCrunch text="QUESTIONS" />
       <ul>
-        <li>
-          <p className="title">Is the wedding outside?</p>
-          <p>
-            Yes.  The whole thing.  It&apos;s all outside.  The reception area will be located under a tent.
-          </p>
-        </li>
-        <li>
-          <p className="title">What if it rains?</p>
-          <p>
-            All festivities will be moved under the tent.
-          </p>
-        </li>
-        <li>
-          <p className="title">What is the attire?</p>
-          <p>
-            Cocktail attire.
-          </p>
-        </li>
-        <li>
-          <p className="title">What is Cocktail attire?</p>
-          <p>
-            Suits and ties for men.  Dresses or pant suits for women.
-          </p>
-          <p>
-            No jeans.  No shorts.  No T-shirts.  No sneakers.
-          </p>
-        </li>
-        <li>
-          <p className="title">When will the official Wedding Invitations be sent?</p>
-          <p>
-            Sometime before the wedding.
-          </p>
-        </li>
-        <li>
-          <p className="title">When should I RSVP by?</p>
-          <p>
-            Before the date listed on your Wedding Invitation (likely May 20th, 2020).
-          </p>
-        </li>
-        <li>
-          <p className="title">Can I bring a guest?</p>
-          <p>
-            IF your invitation says "GUEST" on it, then you may bring a guest.  If you would really like to bring a guest that 
-            was not invited, please speak with us.
-          </p>
-        </li>
-        <li>
-          <p className="title">Can I bring my dog since it is outside?</p>
-          <p>
-            No.  Phipps does not allow dogs in the gardens.
-          </p>
-        </li>
-        <li>
-          <p className="title">Can Brandon McDonald wear basketball shorts?</p>
-          <p>
-            Hell no.  He is required to wear a tuxedo.
-          </p>
-        </li>
+        {
+          faqList.map(faqItem => {
+            return (
+              <li>
+                <p className="title">{ faqItem.question }</p>
+                <p>{ faqItem.answer }</p>
+              </li>
+            )
+          })
+        }
       </ul>
       <div className="stretch-container-16">
         <img src={EdgarCreation} alt="The Creation of Edgar" />
---
+++ app/containers/FAQPage/faqContent.js
@@ -0,0 +1,50 @@
+export const faqList = [
+    {
+        question: 'Where is the wedding?',
+        answer: 'Phipps Conservatory and Botanical Gardens.'
+    },
+    {
+        question: 'Is the wedding outside?',
+        answer: 'Yes.  Everything will be outside.  The reception area will be located under a tent.'
+    },
+    {
+        question: 'What if it rains?',
+        answer: 'All festivities will be moved under the tent.'
+    },
+    {
+        question: 'What is the attire?',
+        answer: 'Cocktail attire.'
+    },
+    {
+        question: 'What is Cocktail attire?',
+        answer: 'Suits and ties for men.  Dresses or pant suits for women.  No jeans.  No shorts.  No T-shirts.  No sneakers. No Viking hats.'
+    },
+    {
+        question: 'When will the official Wedding Invitations be sent?',
+        answer: 'Expect your invitation in February or March 2020.'
+    },
+    {
+        question: 'When should I RSVP by?',
+        answer: 'Before the date listed on your Wedding Invitation (likely May 20th, 2020). Earlier responses are welcome!'
+    },
+    {
+        question: 'Can I bring a guest?',
+        answer: 'IF your invitation says "GUEST" on it, then you may bring a guest.  If you would really like to bring a guest that was not invited, please speak with us.'
+    },
+    {
+        question: 'Can I bring my kid(s)?',
+        answer: 'IF your children are on the invitation then you may bring them. There will not be any childcare services. You will be entirely responsible for their supervision to ensure they do not stick their hands in the wedding cake before it is cut. If you need any special accomodations like a high chair let us know!'
+    },
+    {
+        question: 'Can I bring my dog?',
+        answer: 'No.  Phipps does not allow dogs in the gardens.'
+    },
+    {
+        question: 'Where should I park at the venue?',
+        answer: 'There is parking available in front of Phipps on Schenley Drive.'
+    },
+    {
+        question: 'Can Brandon McDonald wear basketball shorts?',
+        answer: 'Hell no.  He is required to wear a tuxedo.'
+    },
+]
